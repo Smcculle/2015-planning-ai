04e7ba742acc00e768aff5e80132f1334661ba97
Edward Garcia <etgarcia@uno.edu>
2015-09-13 14:48:16 -0500

    The initial implementation of the NormalForm.java. What's next: Write JUnit Test.

--- Planning/src/edu/uno/ai/planning/logic/NormalForms.java
+++ Planning/src/edu/uno/ai/planning/logic/NormalForms.java
@@ -1,9 +1,14 @@
 package edu.uno.ai.planning.logic;
 
+import java.util.ArrayList;
+
+import edu.uno.ai.planning.util.ImmutableArray;
+
 /**
  * Utility methods for converting expressions to conjunctive normal form.
  * 
  * @author Stephen G. Ware
+ * @author Edward Thomas Garcia
  */
 class NormalForms {
 
@@ -14,7 +19,7 @@ class NormalForms {
 	 * @return true if the expression is a literal, false otherwise
 	 */
 	public static boolean isLiteral(Expression expression) {
-		return true;
+		return expression instanceof Literal;
 	}
 	
 	/**
@@ -24,7 +29,28 @@ class NormalForms {
 	 * @return true if the expression is a clause, false otherwise
 	 */
 	public static boolean isClause(Expression expression) {
-		return true;
+		if (expression instanceof Literal)
+			return true;
+		
+		if (expression instanceof NAryBooleanExpression)
+		{
+			NAryBooleanExpression nAryBooleanExpression = (NAryBooleanExpression) expression;
+			for (Expression argument : nAryBooleanExpression.arguments)
+				if (!isLiteral(argument))
+					return false;
+			
+			return true;
+		}
+		
+		// TODO: Check i
+		if (expression instanceof Negation)
+		{
+			Negation negation = (Negation)expression;
+			if (isLiteral(negation.argument))
+				return true;
+		}
+		
+		return false;
 	}
 	
 	/**
@@ -35,7 +61,11 @@ class NormalForms {
 	 * @return true if the expression is a conjunctive clause, false otherwise
 	 */
 	public static boolean isConjunctiveClause(Expression expression) {
-		return true;
+		if (isClause(expression))
+			if (expression instanceof Conjunction)
+				return true;
+		
+		return false;
 	}
 	
 	/**
@@ -46,7 +76,11 @@ class NormalForms {
 	 * @return true if the expression is a disjunctive clause, false otherwise
 	 */
 	public static boolean isDisjunctiveClause(Expression expression) {
-		return true;
+		if (isClause(expression))
+			if (expression instanceof Disjunction)
+				return true;
+		
+		return false;
 	}
 	
 	/**
@@ -57,7 +91,19 @@ class NormalForms {
 	 * @return true if the expression is in CNF, false otherwise
 	 */
 	public static boolean isCNF(Expression expression) {
-		return true;
+		if (expression instanceof Literal)
+			return true;
+		
+		if (expression instanceof Conjunction)
+		{
+			Conjunction conjunction = (Conjunction)expression;
+			for (Expression argument : conjunction.arguments)
+				if (!isDisjunctiveClause(argument))
+					return false;
+			
+			return true;
+		}
+		return false;
 	}
 	
 	/**
@@ -68,7 +114,19 @@ class NormalForms {
 	 * @return true if the expression is in DNF, false otherwise
 	 */
 	public static boolean isDNF(Expression expression) {
-		return true;
+		if (expression instanceof Literal)
+			return true;
+		
+		if (expression instanceof Disjunction)
+		{
+			Disjunction disjunction = (Disjunction)expression;
+			for (Expression argument : disjunction.arguments)
+				if (!isConjunctiveClause(argument))
+					return false;
+			
+			return true;
+		}
+		return false;
 	}
 	
 	/**
@@ -78,9 +136,18 @@ class NormalForms {
 	 * @return an expression in CNF
 	 */
 	public static Expression toCNF(Conjunction conjunction) {
-		return conjunction;
+		if (isCNF(conjunction))
+			return conjunction;
+		
+		Expression conjunctiveNormalForm = convertToNegationNormalForm(conjunction);
+		conjunctiveNormalForm = standardizeVariables(conjunctiveNormalForm);
+		conjunctiveNormalForm = skolemizeStatement(conjunctiveNormalForm);
+		conjunctiveNormalForm = dropAllUniversalQuantifiers(conjunctiveNormalForm);
+		conjunctiveNormalForm = distributeOrOverAnd(conjunctiveNormalForm);
+		
+		return conjunctiveNormalForm;
 	}
-	
+
 	/**
 	 * Converts any {@link Disjunction} to disjunctive normal form.
 	 * 
@@ -88,9 +155,18 @@ class NormalForms {
 	 * @return an expression in DNF
 	 */
 	public static Expression toDNF(Disjunction disjunction) {
-		return disjunction;
+		if (isDNF(disjunction))
+			return disjunction;
+		
+		Expression disjunctiveNormalForm = convertToNegationNormalForm(disjunction);
+		disjunctiveNormalForm = standardizeVariables(disjunctiveNormalForm);
+		disjunctiveNormalForm = skolemizeStatement(disjunctiveNormalForm);
+		disjunctiveNormalForm = dropAllUniversalQuantifiers(disjunctiveNormalForm);
+		disjunctiveNormalForm = distributeAndOverOr(disjunctiveNormalForm);
+		
+		return disjunctiveNormalForm;
 	}
-	
+
 	/**
 	 * Converts any {@link Disjunction} to conjunctive normal form.
 	 * 
@@ -98,7 +174,7 @@ class NormalForms {
 	 * @return an expression in CNF
 	 */
 	public static Expression toCNF(Disjunction disjunction) {
-		return disjunction;
+		return toCNF(new Conjunction(disjunction));
 	}
 	
 	/**
@@ -108,6 +184,157 @@ class NormalForms {
 	 * @return an expression in DNF
 	 */
 	public static Expression toDNF(Conjunction conjunction) {
-		return conjunction;
+		return toDNF(new Disjunction(conjunction));
+	}
+	
+	private static Expression convertToNegationNormalForm(Expression expression) {
+		Expression negationNormalForm = eliminateImplicationsAndEquivalancies(expression);
+		negationNormalForm = moveNotInwards(negationNormalForm);
+		return negationNormalForm;
+	}	
+	
+	private static Expression eliminateImplicationsAndEquivalancies(Expression expression) {
+		return expression;
+	}
+	
+	private static Expression moveNotInwards(Expression expression) {
+		if (isLiteral(expression))
+			return expression;
+		
+		if (expression instanceof Negation)
+		{
+			Expression argument = ((Negation) expression).argument;
+			if (argument instanceof Negation)
+				return moveNotInwards(((Negation)argument).argument);
+			else if (argument instanceof Conjunction)
+				return moveNotInwards(new Disjunction(((Conjunction) argument).negateArguments()));
+			else if (argument instanceof Disjunction)
+				return moveNotInwards(new Conjunction(((Disjunction) argument).negateArguments()));
+			// else handle universal and existential quantifiers if necessary
+		}
+		
+		ImmutableArray<Expression> arguments = null;
+		if (expression instanceof Conjunction) arguments = ((Conjunction) expression).arguments;
+		else if (expression instanceof Disjunction) arguments = ((Disjunction) expression).arguments;
+		
+		if (arguments != null)
+		{
+			ArrayList<Expression> newArguments = new ArrayList<Expression>();
+			for (Expression argument : arguments)
+				if (argument instanceof Negation)
+					newArguments.add(moveNotInwards(argument));
+				else
+					newArguments.add(argument);
+			
+			if (expression instanceof Conjunction)
+				return new Conjunction((Expression[])newArguments.toArray());
+			else if (expression instanceof Disjunction)
+				return new Disjunction((Expression[])newArguments.toArray());
+		}
+		return expression;
+	}
+	
+	private static Expression standardizeVariables(Expression expression) {
+		return expression;
+	}
+	
+	private static Expression skolemizeStatement(Expression expression) {
+		return expression;
+	}
+	
+	private static Expression dropAllUniversalQuantifiers(Expression expression) {
+		return expression;
+	}
+
+	private static Expression distributeOrOverAnd(Expression expression)
+	{
+		expression = expression.simplify();
+		
+		if (isConjunctiveClause(expression))
+			return expression;
+		if (isDisjunctiveClause(expression))
+			return expression;
+		
+		if (expression instanceof Disjunction)
+		{
+			Conjunction complexConjunction = null;
+			for (Expression argument : ((Disjunction) expression).arguments)
+				if (!isLiteral(argument))
+					if (argument instanceof Conjunction)
+						if (((Conjunction) argument).arguments.length > 1)
+						{
+							complexConjunction = (Conjunction)argument;
+							break;
+						}
+			
+			ArrayList<Expression> arguments = new ArrayList<Expression>();
+			for (Expression argument : ((Disjunction) expression).arguments)
+				if (argument != complexConjunction)
+					arguments.add(argument);
+			Expression withoutComplexConjunction = new Disjunction((Expression[])arguments.toArray());
+			
+			ArrayList<Expression> newArguments = new ArrayList<Expression>();
+			for (Expression argument : complexConjunction.arguments)
+				newArguments.add(distributeOrOverAnd(new Disjunction(argument, withoutComplexConjunction)).simplify());
+				
+			return new Conjunction((Expression[])newArguments.toArray()).simplify();
+			
+		}
+		
+		if (expression instanceof Conjunction)
+		{
+			ArrayList<Expression> newArguments = new ArrayList<Expression>();
+			for (Expression argument : ((Conjunction)expression).arguments)
+				newArguments.add(distributeOrOverAnd(argument));
+				
+			return new Conjunction((Expression[])newArguments.toArray()).simplify();
+		}
+		return expression;
+	}
+	
+	private static Expression distributeAndOverOr(Expression expression)
+	{
+		expression = expression.simplify();
+		
+		if (isDisjunctiveClause(expression))
+			return expression;
+		if (isConjunctiveClause(expression))
+			return expression;
+		
+		if (expression instanceof Conjunction)
+		{
+			Disjunction complexDisjunction = null;
+			for (Expression argument : ((Conjunction) expression).arguments)
+				if (!isLiteral(argument))
+					if (argument instanceof Disjunction)
+						if (((Disjunction) argument).arguments.length > 1)
+						{
+							complexDisjunction = (Disjunction)argument;
+							break;
+						}
+			
+			ArrayList<Expression> arguments = new ArrayList<Expression>();
+			for (Expression argument : ((Conjunction) expression).arguments)
+				if (argument != complexDisjunction)
+					arguments.add(argument);
+			Expression withoutComplexDisjunction = new Conjunction((Expression[])arguments.toArray());
+			
+			ArrayList<Expression> newArguments = new ArrayList<Expression>();
+			for (Expression argument : complexDisjunction.arguments)
+				newArguments.add(distributeAndOverOr(new Conjunction(argument, withoutComplexDisjunction)).simplify());
+				
+			return new Disjunction((Expression[])newArguments.toArray()).simplify();
+			
+		}
+		
+		if (expression instanceof Disjunction)
+		{
+			ArrayList<Expression> newArguments = new ArrayList<Expression>();
+			for (Expression argument : ((Disjunction)expression).arguments)
+				newArguments.add(distributeAndOverOr(argument));
+				
+			return new Disjunction((Expression[])newArguments.toArray()).simplify();
+		}
+		return expression;
 	}
 }
