0efdbcc830663582225d7f9f12285a29cfef9ee2
Matthew Farmer <87nolaguy@gmail.com>
2015-10-03 17:48:48 -0500

    adding tesast for threat and updating comments

--- Partial Order Planner/src/edu/uno/ai/planning/pop/CausalLink.java
+++ Partial Order Planner/src/edu/uno/ai/planning/pop/CausalLink.java
@@ -9,8 +9,9 @@ import edu.uno.ai.planning.logic.Predication;
  * @author Matthew Farmer
  * CausalLink object representation class.
  * Each causal link specifies a pair of steps and a proposition, where the 
- *  proposition is a postcondition of the first step and a precondition of the
- *  second step. The first step is ordered before the second step
+ *  proposition is a postcondition of the first operator and a precondition
+ *  of the second operator. The first operator is ordered before the second 
+ *  operator
  */
 public class CausalLink {
 	
--- Partial Order Planner/src/edu/uno/ai/planning/pop/Threat.java
+++ Partial Order Planner/src/edu/uno/ai/planning/pop/Threat.java
@@ -3,14 +3,14 @@
  */
 package edu.uno.ai.planning.pop;
 import edu.uno.ai.planning.Operator;
-import edu.uno.ai.planning.Step;
 
 /**
  * @author Matthew Farmer
- * Extends Flaw interface because Threats and open preconditions
+ * Implements Flaw interface because Threats and OpenPreconditions
  *  are both Flaws that the plan needs to handle.
- * A Threat is an object that represents a step that endangers a
- *  causal link between two other steps.
+ * A Threat is an object that represents the relationship between a 
+ *  step or operator that endangers a causal link between two other
+ *  steps or operators.
  */
 public class Threat implements Flaw {
 	CausalLink threatenedLink;
@@ -20,5 +20,4 @@ public class Threat implements Flaw {
 		this.threatenedLink = threatenedLink;
 		this.threateningOperator = threateningOperator;
 	}
-	
 }
