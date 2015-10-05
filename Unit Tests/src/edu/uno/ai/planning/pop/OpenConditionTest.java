2bdadfdf29fb43099364d017bd16d82839b30b98
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-04 22:55:29 -0500

    Update OpenCondition to hold a PartialStep, not a Step.

--- Unit Tests/src/edu/uno/ai/planning/pop/OpenConditionTest.java
+++ Unit Tests/src/edu/uno/ai/planning/pop/OpenConditionTest.java
@@ -2,42 +2,27 @@ package edu.uno.ai.planning.pop;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-import edu.uno.ai.planning.Settings;
-import edu.uno.ai.planning.Step;
+import org.junit.*;
+
 import edu.uno.ai.planning.logic.*;
-import org.junit.Test;
 
 public class OpenConditionTest {
 	private Class<OpenCondition> describedClass() {
 		return OpenCondition.class;
 	}
 
-	private Literal literal() {
-		return new Predication(
-			"Open Condition Predicate",
-			new Constant(Settings.DEFAULT_TYPE, "A")
-		);
-	}
-	
-	private Step step() {
-		return new Step(
-			"Open Condition Step",
-			new Conjunction(literal()),
-			new Conjunction(literal())
-		);
-	}
-
 	private OpenCondition openCondition() {
-		return new OpenCondition(literal(), step());
+		return new OpenCondition(mock(Literal.class), mock(PartialStep.class));
 	}
 
 	@Test public void has_a_literal() {
 		assertThat(openCondition().literal(), instanceOf(Literal.class));
 	}
-	
+
 	@Test public void has_a_step() {
-		assertThat(openCondition().step(), instanceOf(Step.class));
+		assertThat(openCondition().step(), instanceOf(PartialStep.class));
 	}
 
 	@Test public void is_a_flaw() {
