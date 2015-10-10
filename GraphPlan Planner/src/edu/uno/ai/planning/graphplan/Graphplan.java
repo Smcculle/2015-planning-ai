5d8804f0bb0344a7c4dfed9292a7cd0248bdfd42
GlobalConfusion <montgomery.connor@gmail.com>
2015-10-10 17:10:46 -0500

    update

--- GraphPlan Planner/src/edu/uno/ai/planning/graphplan/Graphplan.java
+++ GraphPlan Planner/src/edu/uno/ai/planning/graphplan/Graphplan.java
@@ -1,9 +1,18 @@
 package edu.uno.ai.planning.graphplan;
 
+import edu.uno.ai.planning.Problem;
+
 public class Graphplan {
 
+	PlanGraph pg;
 	
-	public Graphplan() {
+	public Graphplan(Problem problem) {
+		pg = new PlanGraph(problem, true);
+	}
+	
+	
+	public void extendGraph(){
+		
 		
 	}
 	
