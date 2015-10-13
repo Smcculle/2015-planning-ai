744defd4e5eddcfba0c44b635f3e4e37a97dcf04
Stephen G. Ware <sgware@gmail.com>
2015-10-13 15:23:42 -0500

    Adding some in-line documentation.

--- Partial Order Planner/src/edu/uno/ai/planning/pop/AStarSearch.java
+++ Partial Order Planner/src/edu/uno/ai/planning/pop/AStarSearch.java
@@ -14,9 +14,12 @@ public class AStarSearch extends Search {
 
 		@Override
 		public int compare(PlanSpaceNode n1, PlanSpaceNode n2) {
+			// Work done = number of steps; Work to be done = number of flaws.
 			int comparison = (n1.steps.length - 2 + n1.flaws.size()) - (n2.steps.length - 2 + n2.flaws.size());
+			// Break ties greedily.
 			if(comparison == 0)
 				comparison = n1.flaws.size() - n2.flaws.size();
+			// Break further ties by preferring older nodes to newer nodes.
 			if(comparison == 0)
 				comparison = n1.id - n2.id;
 			return comparison;
@@ -51,8 +54,10 @@ public class AStarSearch extends Search {
 	public Plan findNextSolution() {
 		while(!queue.isEmpty()) {
 			PlanSpaceNode node = queue.poll();
+			// A node with no flaws is a solution.
 			if(node.flaws.size() == 0)
 				return makeSolution(node);
+			// If flaws exist, expand the children of this node.
 			else
 				node.expand(queue);
 		}
