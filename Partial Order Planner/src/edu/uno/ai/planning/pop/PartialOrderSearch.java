d8eb573c88e234e3adb8656efc865890531692fd
dpeabody <dpeabody@uno.edu>
2015-10-14 18:22:50 -0500

    Commented out some stuff in the main to make debugging eassier

--- Partial Order Planner/src/edu/uno/ai/planning/pop/PartialOrderSearch.java
+++ Partial Order Planner/src/edu/uno/ai/planning/pop/PartialOrderSearch.java
@@ -61,7 +61,7 @@ public class PartialOrderSearch extends Search {
 	private Plan pop(){
 		TotalOrderPlan plan = null;
 
-		while(!this.pQueue.isEmpty() && this.nodesVisited < this.limit){
+		while(!this.pQueue.isEmpty() && this.nodesVisited < 20000){
 			PartialOrderNode workingNode = this.pQueue.poll(); // get the node to work on next
 			if(workingNode.flaws.length == 0){
 				plan = workingNode.toTotalOrderPlan();
