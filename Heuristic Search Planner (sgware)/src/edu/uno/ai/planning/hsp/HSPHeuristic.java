cae5acfbbf9cb429d3cceeff6e66f2f8f95b8ecf
Stephen G. Ware <sgware@uno.edu>
2015-11-03 20:26:15 -0600

    HSP solves all problems.

--- Heuristic Search Planner (sgware)/src/edu/uno/ai/planning/hsp/HSPHeuristic.java
+++ Heuristic Search Planner (sgware)/src/edu/uno/ai/planning/hsp/HSPHeuristic.java
@@ -1,6 +1,10 @@
 package edu.uno.ai.planning.hsp;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.function.Consumer;
 
 import edu.uno.ai.planning.State;
 import edu.uno.ai.planning.Step;
@@ -9,14 +13,32 @@ import edu.uno.ai.planning.logic.Conjunction;
 import edu.uno.ai.planning.logic.Expression;
 import edu.uno.ai.planning.logic.Literal;
 import edu.uno.ai.planning.ss.StateSpaceProblem;
+import edu.uno.ai.planning.util.ImmutableArray;
 
 public abstract class HSPHeuristic extends StateHeuristic {
 
 	public static final int INFINITY = Integer.MAX_VALUE;
+	private final ImmutableArray<Literal> literals;
 	private final HashMap<Literal, Integer> cost = new HashMap<>();
 	
 	public HSPHeuristic(StateSpaceProblem problem) {
 		super(problem);
+		HashSet<Literal> literals = new HashSet<>();
+		for(Step step : problem.steps) {
+			everyLiteral(step.precondition, literal -> literals.add(literal));
+			everyLiteral(step.effect, literal -> literals.add(literal));
+		}
+		this.literals = new ImmutableArray<>(literals.toArray(new Literal[literals.size()]));
+	}
+	
+	private static final void everyLiteral(Expression expression, Consumer<Literal> consumer) {
+		if(expression instanceof Literal)
+			consumer.accept((Literal) expression);
+		else if(expression instanceof Conjunction)
+			for(Expression argument : ((Conjunction) expression).arguments)
+				everyLiteral(argument, consumer);
+		else
+			throw new UnsupportedOperationException(expression.getClass() + " not supported.");
 	}
 	
 	protected final int cost(Literal literal) {
@@ -32,16 +54,24 @@ public abstract class HSPHeuristic extends StateHeuristic {
 	@Override
 	public double evaluate(State current) {
 		cost.clear();
+		for(Literal literal : literals) {
+			if(current.isTrue(literal))
+				cost.put(literal, 0);
+		}
+		LinkedList<Step> steps = new LinkedList<>();
 		for(Step step : problem.steps)
-			if(step.precondition.isTrue(current))
-				setCost(step.precondition, 0);
+			steps.add(step);
 		boolean again = true;
 		while(again && cost(problem.goal) == INFINITY) {
 			again = false;
-			for(Step step : problem.steps) {
+			Iterator<Step> iterator = steps.iterator();
+			while(iterator.hasNext()) {
+				Step step = iterator.next();
 				int cost = cost(step.precondition);
-				if(cost != INFINITY)
+				if(cost != INFINITY) {
 					again = setCost(step.effect, cost + 1) || again;
+					iterator.remove();
+				}
 			}
 		}
 		return cost(problem.goal);
@@ -52,7 +82,7 @@ public abstract class HSPHeuristic extends StateHeuristic {
 			Literal literal = (Literal) expression;
 			int current = cost(literal);
 			if(value < current) {
-				cost.put((Literal) expression, value);
+				cost.put(literal, value);
 				return true;
 			}
 			else
