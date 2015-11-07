543fdd41cde2c2177c1a4b945eb2625debefcbd1
GlobalConfusion <montgomery.connor@gmail.com>
2015-11-06 18:02:46 -0600

    Master part 1

--- Unit Tests/src/edu/uno/ai/planning/AllTests.java
+++ Unit Tests/src/edu/uno/ai/planning/AllTests.java
@@ -4,10 +4,21 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ edu.uno.ai.planning.logic.HashBindingsTest.class,
-		edu.uno.ai.planning.logic.ListBindingsTest.class, 
-		edu.uno.ai.planning.logic.NormalFormsTest.class,
-		edu.uno.ai.planning.graphplan.PlanGraphTest.class,
-		edu.uno.ai.planning.graphplan.GraphPlanTest.class})
+@Suite.SuiteClasses({
+	edu.uno.ai.planning.util.LiteralCollectorTest.class,
+	edu.uno.ai.planning.logic.HashBindingsTest.class,
+	edu.uno.ai.planning.logic.ListBindingsTest.class,
+	edu.uno.ai.planning.logic.NormalFormsTest.class,
+	edu.uno.ai.planning.pop.DirectedAcyclicGraphTest.class,
+	edu.uno.ai.planning.pop.POPGraphTest.class,
+	edu.uno.ai.planning.pop.FlawTest.class,
+	edu.uno.ai.planning.pop.OpenConditionTest.class,
+	edu.uno.ai.planning.pop.ThreatTest.class,
+	edu.uno.ai.planning.pop.CloneTypeMismatchExceptionTest.class,
+	edu.uno.ai.planning.graphplan.PlanGraphTest.class,
+	edu.uno.ai.planning.graphplan.PlanGraphStepTest.class,
+	edu.uno.ai.planning.graphplan.PlanGraphLiteralTest.class,
+	edu.uno.ai.planning.graphplan.GraphPlanTest.class,
+})
 public class AllTests {
 	/* nothing */}
