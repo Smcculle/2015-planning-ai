97213763971090b00758feb5b0fe032b3d4395da
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-24 18:20:55 -0600

    Add Partial Order Planner with Unsafe and Local Open Conditions project

---
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/Flaw.java
@@ -0,0 +1,5 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+public interface Flaw extends Partial {
+
+}
---
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/OpenPreconditionFlaw.java
@@ -0,0 +1,26 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import edu.uno.ai.planning.logic.Bindings;
+import edu.uno.ai.planning.logic.Literal;
+import edu.uno.ai.planning.logic.Substitution;
+
+public class OpenPreconditionFlaw implements Flaw {
+
+	public final Step step;
+	public final Literal precondition;
+	
+	OpenPreconditionFlaw(Step step, Literal precondition) {
+		this.step = step;
+		this.precondition = precondition;
+	}
+	
+	@Override
+	public String toString() {
+		return toString(Bindings.EMPTY);
+	}
+
+	@Override
+	public String toString(Substitution substitution) {
+		return precondition.substitute(substitution) + " open for " + step.toString(substitution);
+	}
+}
