da174b0d8b1741e465b7b4f2f17a57eeb7840e96
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-24 18:20:55 -0600

    Add Partial Order Planner with Unsafe and Local Open Conditions project

---
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/Partial.java
@@ -0,0 +1,8 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import edu.uno.ai.planning.logic.Substitution;
+
+public interface Partial {
+
+	public String toString(Substitution substitution);
+}
---
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceRoot.java
@@ -0,0 +1,18 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import edu.uno.ai.planning.Problem;
+
+public class PlanSpaceRoot extends PlanSpaceNode {
+	
+	public final Problem problem;
+	int limit = -1;
+	
+	PlanSpaceRoot(Problem problem) {
+		super(problem);
+		this.problem = problem;
+	}
+	
+	void setNodeLimit(int limit) {
+		this.limit = limit;
+	}
+}
