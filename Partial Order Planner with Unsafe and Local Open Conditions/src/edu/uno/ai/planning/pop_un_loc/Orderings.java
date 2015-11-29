353aad584f3c0b06042f6b8ad1b065cb5919c08d
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-29 15:42:17 -0600

    Add OrderingsTest - can_tell_if_a_step_is_orered_before_another_step()

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/Orderings.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/Orderings.java
@@ -9,150 +9,170 @@ import edu.uno.ai.planning.util.ImmutableList;
 
 public class Orderings implements Iterable<Step>, Partial {
 
-	private final class Node implements Partial {
+  private final class Node implements Partial {
 
-		public final Step step;
-		public final ImmutableList<Step> children;
+    public final Step step;
+    public final ImmutableList<Step> children;
 
-		private Node(Step step, ImmutableList<Step> children) {
-			this.step = step;
-			this.children = children;
-		}
+    private Node(Step step, ImmutableList<Step> children) {
+      this.step = step;
+      this.children = children;
+    }
 
-		public Node(Step step) {
-			this(step, new ImmutableList<>());
-		}
+    public Node(Step step) {
+      this(step, new ImmutableList<>());
+    }
 
-		@Override
-		public String toString() {
-			return toString(Bindings.EMPTY);
-		}
+    @Override
+    public String toString() {
+      return toString(Bindings.EMPTY);
+    }
 
-		@Override
-		public String toString(Substitution substitution) {
-			return step.toString(substitution);
-		}
+    @Override
+    public String toString(Substitution substitution) {
+      return step.toString(substitution);
+    }
 
-		public Node addEdgeTo(Step child) {
-			return new Node(step, children.add(child));
-		}
-	}
+    public Node addEdgeTo(Step child) {
+      return new Node(step, children.add(child));
+    }
+  }
 
-	private final ImmutableList<Node> nodes;
+  private final ImmutableList<Node> nodes;
 
-	private Orderings(ImmutableList<Node> nodes) {
-		this.nodes = nodes;
-	}
+  private Orderings(ImmutableList<Node> nodes) {
+    this.nodes = nodes;
+  }
 
-	Orderings() {
-		this(new ImmutableList<Node>());
-	}
+  Orderings() {
+    this(new ImmutableList<Node>());
+  }
 
-	@Override
-	public String toString() {
-		return toString(Bindings.EMPTY);
-	}
+  @Override
+  public String toString() {
+    return toString(Bindings.EMPTY);
+  }
 
-	@Override
-	public String toString(Substitution substitution) {
-		String str = "ORDERINGS:";
-		for(Node before : nodes)
-			for(Step after : before.children)
-				str += "\n  " + before.toString(substitution) + " < " + after.toString(substitution);
-		return str;
-	}
+  @Override
+  public String toString(Substitution substitution) {
+    String str = "ORDERINGS:";
+    for(Node before : nodes)
+      for(Step after : before.children)
+        str += "\n  " + before.toString(substitution) + " < " + after.toString(substitution);
+    return str;
+  }
 
-	private final Node getNode(Step step) {
-		ImmutableList<Node> current = nodes;
-		while(current != null && current.length != 0) {
-			if(current.first.step == step)
-				return current.first;
-			current = current.rest;
-		}
-		return null;
-	}
+  private final Node getNode(Step step) {
+    ImmutableList<Node> current = nodes;
+    while(current != null && current.length != 0) {
+      if(current.first.step == step)
+        return current.first;
+      current = current.rest;
+    }
+    return null;
+  }
 
-	public Orderings add(Step before, Step after) {
-		Orderings result = this;
-		Node beforeNode = getNode(before);
-		if(beforeNode == null) {
-			beforeNode = new Node(before);
-			result = new Orderings(result.nodes.add(beforeNode));
-		}
-		Node afterNode = getNode(after);
-		if(afterNode == null) {
-			afterNode = new Node(after);
-			result = new Orderings(result.nodes.add(afterNode));
-		}
-		// Would this ordering create a cycle?
-		if(path(afterNode, before))
-			return null;
-		// Does this ordering already exist?
-		if(path(beforeNode, after))
-			return this;
-		beforeNode = beforeNode.addEdgeTo(after);
-		result = new Orderings(replace(beforeNode, result.nodes));
-		return result;
-	}
+  public Orderings add(Step before, Step after) {
+    Orderings result = this;
+    Node beforeNode = getNode(before);
+    if(beforeNode == null) {
+      beforeNode = new Node(before);
+      result = new Orderings(result.nodes.add(beforeNode));
+    }
+    Node afterNode = getNode(after);
+    if(afterNode == null) {
+      afterNode = new Node(after);
+      result = new Orderings(result.nodes.add(afterNode));
+    }
+    // Would this ordering create a cycle?
+    if(path(afterNode, before))
+      return null;
+    // Does this ordering already exist?
+    if(path(beforeNode, after))
+      return this;
+    beforeNode = beforeNode.addEdgeTo(after);
+    result = new Orderings(replace(beforeNode, result.nodes));
+    return result;
+  }
 
-	public boolean allows(Step before, Step middle, Step after) {
-		Orderings newOrderings = this;
-		if(before != middle)
-			newOrderings = newOrderings.add(before, middle);
-		if(newOrderings == null)
-			return false;
-		newOrderings = newOrderings.add(middle, after);
-		return newOrderings != null;
-	}
-	
-	private final boolean path(Node from, Step to) {
-		if(from.step == to)
-			return true;
-		ImmutableList<Step> children = from.children;
-		while(children.length != 0) {
-			if(path(getNode(children.first), to))
-				return true;
-			children = children.rest;
-		}
-		return false;
-	}
+  public boolean allows(Step before, Step middle, Step after) {
+    Orderings newOrderings = this;
+    if(before != middle)
+      newOrderings = newOrderings.add(before, middle);
+    if(newOrderings == null)
+      return false;
+    newOrderings = newOrderings.add(middle, after);
+    return newOrderings != null;
+  }
 
-	private static final ImmutableList<Node> replace(Node node, ImmutableList<Node> nodes) {
-		if(nodes.first.step == node.step)
-			return nodes.rest.add(node);
-		else
-			return replace(node, nodes.rest).add(nodes.first);
-	}
+  public boolean hasOrdering(Step from, Step to) {
+  if (!hasSteps(from, to)) {
+    return false;
+  }
+    return path(getNode(from), to);
+  }
 
-	@Override
-	public Iterator<Step> iterator() {
-		return topologicalSort().iterator();
-	}
+  public boolean hasStep(Step step) {
+    return getNode(step) != null;
+  }
 
-	private final LinkedList<Step> topologicalSort() {
-		LinkedList<Node> nodes = new LinkedList<>();
-		dfs(findStartNode(this.nodes), nodes);
-		LinkedList<Step> steps = new LinkedList<>();
-		for(Node node : nodes)
-			steps.add(node.step);
-		return steps;
-	}
+  public boolean hasSteps(Step... steps) {
+    for (Step step : steps) {
+      if (!hasStep(step)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	private final Node findStartNode(ImmutableList<Node> nodes) {
-		if(nodes.first.step.isStart())
-			return nodes.first;
-		else
-			return findStartNode(nodes.rest);
-	}
+  private final boolean path(Node from, Step to) {
+    if(from.step == to)
+      return true;
+    ImmutableList<Step> children = from.children;
+    while(children.length != 0) {
+      if(path(getNode(children.first), to))
+        return true;
+      children = children.rest;
+    }
+    return false;
+  }
 
-	private final void dfs(Node node, LinkedList<Node> nodes) {
-		if(nodes.contains(node))
-			return;
-		ImmutableList<Step> children = node.children;
-		while(children.length != 0) {
-			dfs(getNode(children.first), nodes);
-			children = children.rest;
-		}
-		nodes.add(0, node);
-	}
+  private static final ImmutableList<Node> replace(Node node, ImmutableList<Node> nodes) {
+    if(nodes.first.step == node.step)
+      return nodes.rest.add(node);
+    else
+      return replace(node, nodes.rest).add(nodes.first);
+  }
+
+  @Override
+  public Iterator<Step> iterator() {
+    return topologicalSort().iterator();
+  }
+
+  private final LinkedList<Step> topologicalSort() {
+    LinkedList<Node> nodes = new LinkedList<>();
+    dfs(findStartNode(this.nodes), nodes);
+    LinkedList<Step> steps = new LinkedList<>();
+    for(Node node : nodes)
+      steps.add(node.step);
+    return steps;
+  }
+
+  private final Node findStartNode(ImmutableList<Node> nodes) {
+    if(nodes.first.step.isStart())
+      return nodes.first;
+    else
+      return findStartNode(nodes.rest);
+  }
+
+  private final void dfs(Node node, LinkedList<Node> nodes) {
+    if(nodes.contains(node))
+      return;
+    ImmutableList<Step> children = node.children;
+    while(children.length != 0) {
+      dfs(getNode(children.first), nodes);
+      children = children.rest;
+    }
+    nodes.add(0, node);
+  }
 }
--- Unit Tests/src/edu/uno/ai/planning/pop_un_loc/OrderingsTest.java
+++ Unit Tests/src/edu/uno/ai/planning/pop_un_loc/OrderingsTest.java
@@ -23,6 +23,40 @@ public class OrderingsTest {
     assertThat(describedClass(), typeCompatibleWith(Iterable.class));
   }
 
+  @Test public void can_tell_if_a_step_is_ordered_before_another_step() {
+    Step start = mock(Step.class);
+    Step end = mock(Step.class);
+    Orderings twoStepOrderings = orderingsWithSteps(start, end);
+
+    assertThat(twoStepOrderings.hasOrdering(start, end), is(true));
+    assertThat(twoStepOrderings.hasOrdering(end, start), is(false));
+
+    assertThat(newEmptyOrderings().hasOrdering(start, end), is(false));
+  }
+
+  @Test public void can_tell_if_a_step_is_included_in_the_orderings() {
+  Step start = mock(Step.class);
+  Step end = mock(Step.class);
+  Step notIncluded = mock(Step.class);
+  Orderings orderings = orderingsWithSteps(start, end);
+
+  assertThat(orderings.hasStep(start), is(true));
+  assertThat(orderings.hasStep(end), is(true));
+  assertThat(orderings.hasStep(notIncluded), is(false));
+  }
+
+  @Test public void can_tell_if_steps_are_included_in_the_orderings() {
+  Step start = mock(Step.class);
+  Step end = mock(Step.class);
+  Step notIncluded = mock(Step.class);
+  Orderings orderings = orderingsWithSteps(start, end);
+
+  assertThat(orderings.hasSteps(start), is(true));
+  assertThat(orderings.hasSteps(start, end), is(true));
+  assertThat(orderings.hasSteps(notIncluded), is(false));
+  assertThat(orderings.hasSteps(start, end, notIncluded), is(false));
+  }
+
   @Test public void cannot_create_cycles() {
     Step start = mock(Step.class);
     Step middle = mock(Step.class);
