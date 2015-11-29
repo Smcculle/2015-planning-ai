027459cf6016aba81c83463da06f9f255d9147d5
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-29 15:06:36 -0600

    Add OrderingsTest - cannot_create_cycles() spec.

--- Unit Tests/src/edu/uno/ai/planning/pop_un_loc/OrderingsTest.java
+++ Unit Tests/src/edu/uno/ai/planning/pop_un_loc/OrderingsTest.java
@@ -1,7 +1,8 @@
 package edu.uno.ai.planning.pop_un_loc;
 
-import static org.hamcrest.Matchers.typeCompatibleWith;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 
@@ -10,7 +11,26 @@ public class OrderingsTest {
     return Orderings.class;
   }
 
+  private Orderings newEmptyOrderings() {
+    return new Orderings();
+  }
+
+  private Orderings orderingsWithSteps(Step first, Step second) {
+    return new Orderings().add(first, second);
+  }
+
   @Test public void implements_iterable_step() {
     assertThat(describedClass(), typeCompatibleWith(Iterable.class));
+  }
+
+  @Test public void cannot_create_cycles() {
+    Step start = mock(Step.class);
+    Step middle = mock(Step.class);
+    Step end = mock(Step.class);
+    Orderings orderings = orderingsWithSteps(start, end)
+                         .add(start, middle)
+                         .add(middle, start);
+
+    assertThat(orderings, is(nullValue()));
   }
 }
