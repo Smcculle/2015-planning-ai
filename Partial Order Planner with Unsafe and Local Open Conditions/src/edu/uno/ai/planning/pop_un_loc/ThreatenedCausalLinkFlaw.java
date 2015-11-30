ed5d8461b9572114c77927c02ea40916e6b3da05
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-30 16:24:17 -0600

    Reformat pop_un_loc.ThreatenedCausalLinkFlaw

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/ThreatenedCausalLinkFlaw.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/ThreatenedCausalLinkFlaw.java
@@ -5,21 +5,22 @@ import edu.uno.ai.planning.logic.Substitution;
 
 public class ThreatenedCausalLinkFlaw implements Flaw {
 
-	public final CausalLink link;
-	public final Step threat;
-	
-	public ThreatenedCausalLinkFlaw(CausalLink link, Step threat) {
-		this.link = link;
-		this.threat = threat;
-	}
-	
-	@Override
-	public String toString() {
-		return toString(Bindings.EMPTY);
-	}
+  public final CausalLink link;
+  public final Step threat;
 
-	@Override
-	public String toString(Substitution substitution) {
-		return threat.toString(substitution) + " threatens " + link.toString(substitution);
-	}
+  public ThreatenedCausalLinkFlaw(CausalLink link, Step threat) {
+    this.link = link;
+    this.threat = threat;
+  }
+
+  @Override
+  public String toString() {
+    return toString(Bindings.EMPTY);
+  }
+
+  @Override
+  public String toString(Substitution substitution) {
+    return threat.toString(substitution) + " threatens " +
+           link.toString(substitution);
+  }
 }
