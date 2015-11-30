7fc4d4f433ae6d616cb477fa4cbb7197dcbbe1be
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-30 16:18:36 -0600

    Reformat pop_un_loc.FlawList

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/FlawList.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/FlawList.java
@@ -7,59 +7,60 @@ import edu.uno.ai.planning.util.ImmutableList;
 
 public class FlawList implements Partial {
 
-	private final ImmutableList<Flaw> flaws;
-	
-	private FlawList(ImmutableList<Flaw> flaws) {
-		this.flaws = flaws;
-	}
-	
-	FlawList(Step end) {
-		ImmutableList<Flaw> flaws = new ImmutableList<>();
-		for(Literal precondition : end.preconditions)
-			flaws = flaws.add(new OpenPreconditionFlaw(end, precondition));
-		this.flaws = flaws;
-	}
-	
-	@Override
-	public String toString() {
-		return toString(Bindings.EMPTY);
-	}
-	
-	@Override
-	public String toString(Substitution substitution) {
-		String str = "FLAWS:";
-		for(Flaw flaw : flaws)
-			str += "\n  " + flaw.toString(substitution);
-		return str;
-	}
-	
-	public int size() {
-		return flaws.length;
-	}
-	
-	public Flaw chooseFlaw() {
-		return flaws.first;
-	}
-	
-	public FlawList add(Flaw flaw) {
-		return new FlawList(flaws.add(flaw));
-	}
-	
-	public FlawList remove(Flaw flaw) {
-		return new FlawList(remove(flaw, flaws));
-	}
-	
-	private static final ImmutableList<Flaw> remove(Flaw flaw, ImmutableList<Flaw> flaws) {
-		if(flaws.length == 0)
-			return flaws;
-		else if(flaws.first == flaw)
-			return flaws.rest;
-		else {
-			ImmutableList<Flaw> rest = remove(flaw, flaws.rest);
-			if(rest == flaws.rest)
-				return flaws;
-			else
-				return rest.add(flaws.first);
-		}
-	}
+  private static final ImmutableList<Flaw> remove(Flaw flaw,
+                                                  ImmutableList<Flaw> flaws) {
+    if (flaws.length == 0)
+      return flaws;
+    else if (flaws.first == flaw)
+      return flaws.rest;
+    else {
+      ImmutableList<Flaw> rest = remove(flaw, flaws.rest);
+      if (rest == flaws.rest)
+        return flaws;
+      else
+        return rest.add(flaws.first);
+    }
+  }
+
+  private final ImmutableList<Flaw> flaws;
+
+  FlawList(Step end) {
+    ImmutableList<Flaw> flaws = new ImmutableList<>();
+    for (Literal precondition : end.preconditions)
+      flaws = flaws.add(new OpenPreconditionFlaw(end, precondition));
+    this.flaws = flaws;
+  }
+
+  private FlawList(ImmutableList<Flaw> flaws) {
+    this.flaws = flaws;
+  }
+
+  public FlawList add(Flaw flaw) {
+    return new FlawList(flaws.add(flaw));
+  }
+
+  public Flaw chooseFlaw() {
+    return flaws.first;
+  }
+
+  public FlawList remove(Flaw flaw) {
+    return new FlawList(remove(flaw, flaws));
+  }
+
+  public int size() {
+    return flaws.length;
+  }
+
+  @Override
+  public String toString() {
+    return toString(Bindings.EMPTY);
+  }
+
+  @Override
+  public String toString(Substitution substitution) {
+    String str = "FLAWS:";
+    for (Flaw flaw : flaws)
+      str += "\n  " + flaw.toString(substitution);
+    return str;
+  }
 }
