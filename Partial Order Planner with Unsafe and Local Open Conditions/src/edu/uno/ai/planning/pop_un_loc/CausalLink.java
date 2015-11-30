eee086d4bd4dbd8f02876e76748f05005e479dd3
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-30 16:15:56 -0600

    Reformat pop_un_loc.CausalLink

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/CausalLink.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/CausalLink.java
@@ -6,23 +6,24 @@ import edu.uno.ai.planning.logic.Substitution;
 
 public class CausalLink implements Partial {
 
-	public final Step tail;
-	public final Literal label;
-	public final Step head;
-	
-	CausalLink(Step tail, Literal label, Step head) {
-		this.tail = tail;
-		this.label = label;
-		this.head = head;
-	}
-	
-	@Override
-	public String toString() {
-		return toString(Bindings.EMPTY);
-	}
+  public final Step head;
+  public final Literal label;
+  public final Step tail;
 
-	@Override
-	public String toString(Substitution substitution) {
-		return tail.toString(substitution) + "-" + label.substitute(substitution) + "->" + head.toString(substitution);
-	}
+  CausalLink(Step tail, Literal label, Step head) {
+    this.tail = tail;
+    this.label = label;
+    this.head = head;
+  }
+
+  @Override
+  public String toString() {
+    return toString(Bindings.EMPTY);
+  }
+
+  @Override
+  public String toString(Substitution substitution) {
+    return tail.toString(substitution) + "-" + label.substitute(substitution) +
+           "->" + head.toString(substitution);
+  }
 }
