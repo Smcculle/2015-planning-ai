4b200185e68ff3f6ba95b2f0e0283d6f076b5d0b
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-30 16:19:34 -0600

    Reformat pop_un_loc.OpenPreconditionFlaw

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/OpenPreconditionFlaw.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/OpenPreconditionFlaw.java
@@ -6,21 +6,22 @@ import edu.uno.ai.planning.logic.Substitution;
 
 public class OpenPreconditionFlaw implements Flaw {
 
-	public final Step step;
-	public final Literal precondition;
-	
-	OpenPreconditionFlaw(Step step, Literal precondition) {
-		this.step = step;
-		this.precondition = precondition;
-	}
-	
-	@Override
-	public String toString() {
-		return toString(Bindings.EMPTY);
-	}
+  public final Literal precondition;
+  public final Step step;
 
-	@Override
-	public String toString(Substitution substitution) {
-		return precondition.substitute(substitution) + " open for " + step.toString(substitution);
-	}
+  OpenPreconditionFlaw(Step step, Literal precondition) {
+    this.step = step;
+    this.precondition = precondition;
+  }
+
+  @Override
+  public String toString() {
+    return toString(Bindings.EMPTY);
+  }
+
+  @Override
+  public String toString(Substitution substitution) {
+    return precondition.substitute(substitution) + " open for " +
+           step.toString(substitution);
+  }
 }
