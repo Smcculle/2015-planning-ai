7b1187722bbf63f59dcd3304705e8b602f8ecfe4
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-30 16:20:27 -0600

    Reformat pop_un_loc.PlanSpaceRoot

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceRoot.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceRoot.java
@@ -3,16 +3,17 @@ package edu.uno.ai.planning.pop_un_loc;
 import edu.uno.ai.planning.Problem;
 
 public class PlanSpaceRoot extends PlanSpaceNode {
-	
-	public final Problem problem;
-	int limit = -1;
-	
-	PlanSpaceRoot(Problem problem) {
-		super(problem);
-		this.problem = problem;
-	}
-	
-	void setNodeLimit(int limit) {
-		this.limit = limit;
-	}
+
+  public final Problem problem;
+
+  int limit = -1;
+
+  PlanSpaceRoot(Problem problem) {
+    super(problem);
+    this.problem = problem;
+  }
+
+  void setNodeLimit(int limit) {
+    this.limit = limit;
+  }
 }
