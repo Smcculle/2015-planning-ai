6666b2584009fef300e225838b357e7e9fa77e7e
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-01 12:14:05 -0600

    Add initial pop_un_loc.Flaws

---
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/Flaws.java
@@ -0,0 +1,13 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import edu.uno.ai.planning.logic.Substitution;
+
+public class Flaws implements Partial {
+
+  @Override
+  public String toString(Substitution substitution) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
