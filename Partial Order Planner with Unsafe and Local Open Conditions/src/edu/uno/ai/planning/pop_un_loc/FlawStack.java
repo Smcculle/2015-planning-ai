054718736719947c968bd8aa61422c39b94eec3f
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-30 21:43:39 -0600

    Add initial pop_un_loc.FlawStack

---
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/FlawStack.java
@@ -0,0 +1,41 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import java.util.Stack;
+
+import edu.uno.ai.planning.logic.Bindings;
+import edu.uno.ai.planning.logic.Substitution;
+
+public class FlawStack implements Partial {
+
+  private Stack<Flaw> flaws;
+
+  public FlawStack() {
+    flaws = new Stack<Flaw>();
+  }
+
+  public FlawStack(Flaw flaw) {
+    flaws = new Stack<Flaw>();
+    push(flaw);
+  }
+
+  public Stack<Flaw> flaws() {
+    return flaws;
+  }
+
+  public void push(Flaw flaw) {
+    flaws.push(flaw);
+  }
+
+  @Override
+  public String toString() {
+    return toString(Bindings.EMPTY);
+  }
+
+  @Override
+  public String toString(Substitution substitution) {
+    String str = "FLAWS:";
+    // for (Flaw flaw : flaws)
+    // str += "\n " + flaw.toString(substitution);
+    return str;
+  }
+}
---
+++ Unit Tests/src/edu/uno/ai/planning/pop_un_loc/FlawStackTest.java
@@ -0,0 +1,133 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.typeCompatibleWith;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.Stack;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.nitorcreations.junit.runners.NestedRunner;
+
+import edu.uno.ai.planning.logic.Substitution;
+
+@RunWith(NestedRunner.class)
+public class FlawStackTest {
+  Class<FlawStack> describedClass() {
+    return FlawStack.class;
+  }
+
+  FlawStack emptyFlawStack() {
+    return new FlawStack();
+  }
+
+  FlawStack flawStackWithFlaw(Flaw flaw) {
+    return new FlawStack(flaw);
+  }
+
+  FlawStack flawStackWithFlaws(Flaw... flaws) {
+    FlawStack result = new FlawStack();
+
+    for (Flaw flaw : flaws) {
+      result.push(flaw);
+    }
+
+    return result;
+  }
+
+  FlawStack flawStack;
+
+  @Before
+  public void setup() {
+    flawStack = emptyFlawStack();
+  }
+
+  @Test
+  public void implements_partial() {
+    assertThat(describedClass(), typeCompatibleWith(Partial.class));
+  }
+
+  public class flaws {
+    Stack<Flaw> flaws;
+
+    public class when_the_flaw_stack_is_empty {
+      @Before
+      public void setup() {
+        flawStack = emptyFlawStack();
+      }
+
+      @Test
+      public void is_an_empty_stack_of_flaws() {
+        assertThat(flawStack.flaws(), equalTo(new Stack<Flaw>()));
+      }
+    }
+
+    public class when_the_flaw_stack_has_one_flaw {
+      Flaw flaw;
+
+      @Before
+      public void setup() {
+        flaw = mock(Flaw.class);
+        flawStack = flawStackWithFlaw(flaw);
+      }
+
+      @Test
+      public void is_a_stack_with_the_one_flaw() {
+        Stack<Flaw> stack = new Stack<Flaw>();
+        stack.push(flaw);
+        assertThat(flawStack.flaws(), equalTo(stack));
+      }
+    }
+
+    public class when_the_flaw_stack_has_two_flaws {
+      Flaw first;
+      Flaw second;
+
+      @Before
+      public void setup() {
+        first = mock(Flaw.class);
+        second = mock(Flaw.class);
+        flawStack = flawStackWithFlaws(first, second);
+      }
+
+      @Test
+      public void is_a_stack_with_the_second_then_the_first_added() {
+        Stack<Flaw> stack = new Stack<Flaw>();
+        stack.push(first);
+        stack.push(second);
+        assertThat(flawStack.flaws(), equalTo(stack));
+      }
+    }
+  }
+
+  public class toString_substitution {
+    Substitution substitution;
+
+    @Before
+    public void setup() {
+      substitution = mock(Substitution.class);
+    }
+
+    @Test
+    public void starts_with_FLAWS() {
+      assertThat(flawStack.toString(substitution), startsWith("FLAWS:"));
+    }
+
+    public class when_the_flaw_stack_is_empty {
+      @Before
+      public void setup() {
+        flawStack = emptyFlawStack();
+      }
+
+      @Test
+      public void is_just_FLAWS() {
+        assertThat(flawStack.toString(substitution), equalTo("FLAWS:"));
+      }
+    }
+  }
+}
