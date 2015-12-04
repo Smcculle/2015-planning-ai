968e91bbf5b73c5d655ccbe24b2ca40f7b4daa15
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-03 18:09:36 -0600

    Add pop_un_loc.AbstractFlaw

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/Flaw.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/Flaw.java
@@ -2,4 +2,9 @@ package edu.uno.ai.planning.pop_un_loc;
 
 public interface Flaw extends Partial {
 
+  public Boolean isOpenCondition();
+
+  public Boolean isThreat();
+
+  public Boolean isUnsafe();
 }
