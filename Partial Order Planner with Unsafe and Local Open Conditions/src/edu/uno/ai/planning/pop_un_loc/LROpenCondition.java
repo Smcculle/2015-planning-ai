b951fc87d0281c9b2597c1a4ddc05d1d641960a1
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-06 17:53:07 -0600

    Add pop_un_loc.LROpenCondition - possibleSteps()

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/LROpenCondition.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/LROpenCondition.java
@@ -1,5 +1,10 @@
 package edu.uno.ai.planning.pop_un_loc;
 
+import edu.uno.ai.planning.Operator;
+import edu.uno.ai.planning.logic.Bindings;
+import edu.uno.ai.planning.logic.Literal;
+import javaslang.collection.List;
+
 public class LROpenCondition extends AbstractLR<OpenCondition> {
   public LROpenCondition(PlanSpaceNode planSpaceNode) {
     super(planSpaceNode);
@@ -10,8 +15,42 @@ public class LROpenCondition extends AbstractLR<OpenCondition> {
     return first;
   }
 
+  public List<Step> possibleSolutions() {
+    return List.ofAll(planSpaceNode().steps()).appendAll(operatorSteps());
+  }
+
+  public List<Step> operatorSteps() {
+    List<Step> result = List.empty();
+
+    for (Operator operator : planSpaceNode().operators())
+      result = result.push(new Step(operator));
+
+    return result;
+  }
+
   @Override
   public Integer refinementsOf(OpenCondition flaw) {
-    return 0;
+    Integer refinementCount = 0;
+
+    for (Step step : possibleSolutions())
+      if (solvesFlaw(step, flaw))
+        refinementCount++;
+
+    return refinementCount;
+  }
+
+  public Boolean solvesFlaw(Step step, OpenCondition flaw) {
+    for (Literal effect : step.effects()) {
+      Bindings newBindings = flaw.precondition()
+                                 .unify(effect, planSpaceNode().bindings());
+      if (newBindings != null) {
+        Orderings newOrderings = planSpaceNode().orderings().add(step,
+                                                                 flaw.step);
+        if (newOrderings != null) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
