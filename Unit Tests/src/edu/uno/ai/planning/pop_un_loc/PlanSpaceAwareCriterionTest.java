6fe813bc882f6cd02cdb8ab47340186a493766d5
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-06 15:06:29 -0600

    Add missing @RunWith(NestedRunner.class)

--- Unit Tests/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceAwareCriterionTest.java
+++ Unit Tests/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceAwareCriterionTest.java
@@ -4,7 +4,11 @@ import static org.hamcrest.Matchers.typeCompatibleWith;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.nitorcreations.junit.runners.NestedRunner;
+
+@RunWith(NestedRunner.class)
 public class PlanSpaceAwareCriterionTest {
   static Class<PlanSpaceAwareCriterion> describedClass() {
     return PlanSpaceAwareCriterion.class;
