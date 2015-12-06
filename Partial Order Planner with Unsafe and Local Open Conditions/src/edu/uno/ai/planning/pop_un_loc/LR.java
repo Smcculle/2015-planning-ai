9b5dd243d5fe0cd711499dbefe5af1a470a13352
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-06 15:28:22 -0600

    Add pop_un_loc.LR

---
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/LR.java
@@ -0,0 +1,8 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+public class LR<T extends Flaw> extends PlanSpaceAwareCriterion<T> {
+
+  LR(PlanSpaceNode planSpaceNode) {
+    super(planSpaceNode);
+  }
+}
---
+++ Unit Tests/src/edu/uno/ai/planning/pop_un_loc/LRTest.java
@@ -0,0 +1,22 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import static org.hamcrest.Matchers.typeCompatibleWith;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.nitorcreations.junit.runners.NestedRunner;
+
+@RunWith(NestedRunner.class)
+public class LRTest {
+  static Class<LR> describedClass() {
+    return LR.class;
+  }
+
+  @Test
+  public void is_a_plan_space_aware_criterion() {
+    assertThat(describedClass(),
+               typeCompatibleWith(PlanSpaceAwareCriterion.class));
+  }
+}
