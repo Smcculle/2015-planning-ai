a25407b3e069f7978711f32938b2af94f131e330
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-06 13:13:22 -0600

    Add pop_un_loc.FlawSelection and PlanSpaceDefinedSelection

---
+++ Unit Tests/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceDefinedSelectionTest.java
@@ -0,0 +1,56 @@
+package edu.uno.ai.planning.pop_un_loc;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.nitorcreations.junit.runners.NestedRunner;
+
+@RunWith(NestedRunner.class)
+public class PlanSpaceDefinedSelectionTest {
+  static Class<PlanSpaceDefinedSelection> describedClass() {
+    return PlanSpaceDefinedSelection.class;
+  }
+
+  PlanSpaceDefinedSelection<Flaw> definition;
+  PlanSpaceNode planSpaceNode;
+  Flaws<Flaw> flaws;
+
+  PlanSpaceDefinedSelection<Flaw> currentDefinition() {
+    return new PlanSpaceDefinedSelection<Flaw>(planSpaceNode, flaws);
+  }
+
+  PlanSpaceDefinedSelection<Flaw> definitionWithEmptySelection() {
+    return definitionWith(mock(PlanSpaceNode.class), new Flaws<Flaw>());
+  }
+
+  PlanSpaceDefinedSelection<Flaw> definitionWith(PlanSpaceNode planSpaceNode,
+                                                 Flaws<Flaw> flaws) {
+    planSpaceNode = mock(PlanSpaceNode.class);
+    flaws = new Flaws<Flaw>();
+    return new PlanSpaceDefinedSelection<Flaw>(planSpaceNode, flaws);
+  }
+
+  @Before
+  public void beforeExample() {
+    definition = definitionWithEmptySelection();
+  }
+
+  public class flaws {
+    @Test
+    public void is_a_collection_of_flaws() {
+      assertThat(definition.flaws(), instanceOf(Flaws.class));
+    }
+  }
+
+  public class planSpaceNode {
+    @Test
+    public void is_the_context_of_the_selection() {
+      assertThat(definition.planSpaceNode(), instanceOf(PlanSpaceNode.class));
+    }
+  }
+}
