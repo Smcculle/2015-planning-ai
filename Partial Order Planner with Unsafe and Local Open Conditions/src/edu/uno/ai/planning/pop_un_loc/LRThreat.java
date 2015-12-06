8509b897c66626e1bc791c01ccd6cee6471c759a
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-06 16:36:15 -0600

    Add pop_un_loc.LRThreat - canBePromoted(flaw)

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/LRThreat.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/LRThreat.java
@@ -14,6 +14,12 @@ public class LRThreat extends AbstractLR<ThreatenedCausalLink> {
     return second;
   }
 
+  public Boolean canBePromoted(ThreatenedCausalLink flaw) {
+    if (planSpaceNode().promoteThreat(flaw) == null)
+      return false;
+    return true;
+  }
+
   @Override
   public Integer refinementsOf(ThreatenedCausalLink flaw) {
     return 0;
