375b54fecb6f10a6d27845550f69dd02a4136468
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-06 13:57:47 -0600

    Add pop_un_loc.PlanSpaceDefinedSelection - selectBy(criterion) query

--- Unit Tests/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceDefinedSelectionTest.java
+++ Unit Tests/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceDefinedSelectionTest.java
@@ -1,6 +1,8 @@
 package edu.uno.ai.planning.pop_un_loc;
 
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
@@ -55,6 +57,43 @@ public class PlanSpaceDefinedSelectionTest {
     @Test
     public void is_the_context_of_the_selection() {
       assertThat(definition.planSpaceNode(), instanceOf(PlanSpaceNode.class));
+    }
+  }
+
+  public class selectedBy_criterion {
+    Criterion<Flaw> criterion;
+
+    @Before
+    public void beforeExample() {
+      criterion = mock(Criterion.class);
+    }
+
+    public class when_there_are_no_flaws {
+      @Before
+      public void beforeExample() {
+        definition = definitionWithEmptySelection();
+      }
+
+      @Test
+      public void is_null() {
+        assertThat(definition.selectedBy(criterion), is(nullValue()));
+      }
+    }
+
+    public class when_there_is_a_flaw {
+      Flaw flaw;
+
+      @Before
+      public void beforeExample() {
+        flaw = mock(Flaw.class);
+        flaws = new Flaws<Flaw>(flaw);
+        definition = definitionWith(flaws);
+      }
+
+      @Test
+      public void is_that_flaw() {
+        assertThat(definition.selectedBy(criterion), is(flaw));
+      }
     }
   }
 }
