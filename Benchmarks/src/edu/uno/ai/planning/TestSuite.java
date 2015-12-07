bf66e0a67d085c3600be686ad892e92de8fcf7cb
Shane McCulley <smcculle@uno.edu>
2015-12-06 20:58:39 -0600

    reverted to earlier commit

--- Benchmarks/src/edu/uno/ai/planning/TestSuite.java
+++ Benchmarks/src/edu/uno/ai/planning/TestSuite.java
@@ -1,7 +1,6 @@
 package edu.uno.ai.planning;
 
 import edu.uno.ai.planning.Planner;
-import edu.uno.ai.planning.bfs.BFSPlanner;
 
 public class TestSuite {
 
@@ -9,7 +8,8 @@ public class TestSuite {
 	public static final long TIME_LIMIT = Planner.NO_TIME_LIMIT;
 	
 	public static final Planner<?>[] PLANNERS = new Planner[]{
-		new BFSPlanner(),
+		new edu.uno.ai.planning.bfs.BFSPlanner(),
+		new edu.uno.ai.planning.lpg.LPGPlanner(),
 	};
 	
 	public static final Benchmark[] BENCHMARKS = new Benchmark[]{
@@ -19,20 +19,20 @@ public class TestSuite {
 		new Benchmark("blocks", "sussman"),
 		new Benchmark("blocks", "reverse_2"),
 		new Benchmark("blocks", "reverse_4"),
-		new Benchmark("blocks", "reverse_6"),
-		new Benchmark("blocks", "reverse_8"),
-		new Benchmark("blocks", "reverse_10"),
-		new Benchmark("blocks", "reverse_12"),
+//		new Benchmark("blocks", "reverse_6"),
+//		new Benchmark("blocks", "reverse_8"),
+//		new Benchmark("blocks", "reverse_10"),
+//		new Benchmark("blocks", "reverse_12"),
 		new Benchmark("cargo", "deliver_1"),
 		new Benchmark("cargo", "deliver_2"),
-		new Benchmark("cargo", "deliver_3"),
-		new Benchmark("cargo", "deliver_4"),
+//		new Benchmark("cargo", "deliver_3"),
+//		new Benchmark("cargo", "deliver_4"),
 		new Benchmark("cargo", "deliver_return_1"),
 		new Benchmark("cargo", "deliver_return_2"),
-		new Benchmark("cargo", "deliver_return_3"),
-		new Benchmark("cargo", "deliver_return_4"),
+//		new Benchmark("cargo", "deliver_return_3"),
+//		new Benchmark("cargo", "deliver_return_4"),
 		new Benchmark("wumpus", "easy_wumpus"),
-		new Benchmark("wumpus", "medium_wumpus"),
-		new Benchmark("wumpus", "hard_wumpus"),
+//		new Benchmark("wumpus", "medium_wumpus"),
+//		new Benchmark("wumpus", "hard_wumpus"),
 	};
 }
