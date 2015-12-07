b7c21c5f85f4a622edd49a7174f7792b314238a3
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-07 00:08:32 -0600

    Fix Benchmarks

--- Benchmarks/src/edu/uno/ai/planning/TestSuite.java
+++ Benchmarks/src/edu/uno/ai/planning/TestSuite.java
@@ -2,48 +2,49 @@ package edu.uno.ai.planning;
 
 public class TestSuite {
 
-	public static final int NODE_LIMIT = 30000;
-	public static final long TIME_LIMIT = Planner.NO_TIME_LIMIT;
+  public static final int NODE_LIMIT = 10000;
+  public static final long TIME_LIMIT = Planner.NO_TIME_LIMIT;
 
-	public static final Planner<?>[] PLANNERS = new Planner[]{
-		new edu.uno.ai.planning.bfs.BFSPlanner(),
-//		new edu.uno.ai.planning.iw.IteratedWidthPlanner(),
-		//new edu.uno.ai.planning.pop.PartialOrderPlanner(),
-		new edu.uno.ai.planning.spop.PartialOrderPlanner(),
-		// new edu.uno.ai.planning.graphplan.Graphplan(),
-		// new edu.uno.ai.planning.gp.GraphPlan(),
-		new edu.uno.ai.planning.hsp.HSPlanner(),
-		new edu.uno.ai.planning.shsp.HeuristicSearchPlanner(),
-		new edu.uno.ai.planning.ff.FastForwardPlanner(),
-		new edu.uno.ai.planning.pop_un_loc.POPUnLOC(),
-	};
+  public static final Planner<?>[] PLANNERS = new Planner[] {
+    new edu.uno.ai.planning.bfs.BFSPlanner(),
+    new edu.uno.ai.planning.iw.IteratedWidthPlanner(),
+    new edu.uno.ai.planning.pop.PartialOrderPlanner(),
+    new edu.uno.ai.planning.spop.PartialOrderPlanner(),
+    new edu.uno.ai.planning.pop_un_loc.POPUnLOC(),
+// new edu.uno.ai.planning.graphplan.Graphplan(),
+    new edu.uno.ai.planning.gp.GraphPlan(),
+// new edu.uno.ai.planning.hsp.HSPlanner(),
+    new edu.uno.ai.planning.shsp.HeuristicSearchPlanner(),
+      new edu.uno.ai.planning.ff.FastForwardPlanner(), };
 
-	public static final Benchmark[] BENCHMARKS = new Benchmark[]{
-		new Benchmark("blocks", "do_nothing"),
-		new Benchmark("blocks", "bw-largest"),
-//		new Benchmark("blocks", "easy_stack"),
-//		new Benchmark("blocks", "easy_unstack"),
-//		new Benchmark("blocks", "sussman"),
-//		new Benchmark("cake", "have_eat_cake"),
-//		new Benchmark("blocks", "reverse_2"),
-//		new Benchmark("blocks", "reverse_4"),
-//		new Benchmark("blocks", "reverse_6"),
-//		new Benchmark("blocks", "reverse_8"),
-//		new Benchmark("blocks", "reverse_10"),
-//		new Benchmark("blocks", "reverse_12"),
-//		new Benchmark("blocks", "tower-invert"),
-//		new Benchmark("road", "road-test"),
-//		new Benchmark("blocks", "bw-large"),
-//		new Benchmark("cargo", "deliver_1"),
-//		new Benchmark("cargo", "deliver_2"),
-//		new Benchmark("cargo", "deliver_3"),
-//		new Benchmark("cargo", "deliver_4"),
-//		new Benchmark("cargo", "deliver_return_1"),
-//		new Benchmark("cargo", "deliver_return_2"),
-//		new Benchmark("cargo", "deliver_return_3"),
-//		new Benchmark("cargo", "deliver_return_4"),
-//		new Benchmark("wumpus", "easy_wumpus"),
-//		new Benchmark("wumpus", "medium_wumpus"),
-//		new Benchmark("wumpus", "hard_wumpus"),
-	};
+  public static final Benchmark[] BENCHMARKS = new Benchmark[] {
+    new Benchmark("blocks","do_nothing"),
+    new Benchmark("blocks","easy_stack"),
+    new Benchmark("blocks","easy_unstack"),
+    new Benchmark("blocks","sussman"),
+    new Benchmark("cake","have_eat_cake"),
+    new Benchmark("blocks","reverse_2"),
+    new Benchmark("blocks","reverse_4"),
+    new Benchmark("blocks","reverse_6"),
+    new Benchmark("blocks","reverse_8"),
+    new Benchmark("blocks","reverse_10"),
+    new Benchmark("blocks","reverse_12"),
+    new Benchmark("blocks","tower-invert"),
+    new Benchmark("road","road-test"),
+    new Benchmark("blocks","bw-large"),
+    new Benchmark("blocks","bw-large3"),
+    new Benchmark("blocks","bw-large2"),
+    new Benchmark("blocks", "bw-largest"),
+    new Benchmark("cargo","deliver_1"),
+    new Benchmark("cargo","deliver_2"),
+    new Benchmark("cargo","deliver_3"),
+    new Benchmark("cargo","deliver_4"),
+    new Benchmark("cargo","deliver_return_1"),
+    new Benchmark("cargo","deliver_return_2"),
+    new Benchmark("cargo","deliver_return_3"),
+    new Benchmark("cargo","deliver_return_4"),
+    new Benchmark("wumpus","easy_wumpus"),
+    new Benchmark("wumpus","medium_wumpus"),
+    new Benchmark("wumpus","hard_wumpus"),
+  };
 }
