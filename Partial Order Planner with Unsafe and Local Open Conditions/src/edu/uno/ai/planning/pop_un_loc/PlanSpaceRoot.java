67f139455540aa04c47b9a3987132a90b1fb8587
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-06 21:21:05 -0600

    Add pop_un_loc.MW

--- Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceRoot.java
+++ Partial Order Planner with Unsafe and Local Open Conditions/src/edu/uno/ai/planning/pop_un_loc/PlanSpaceRoot.java
@@ -2,16 +2,20 @@ package edu.uno.ai.planning.pop_un_loc;
 
 import edu.uno.ai.planning.Problem;
 import edu.uno.ai.planning.SearchLimitReachedException;
+import edu.uno.ai.planning.shsp.AdditiveHeuristic;
+import edu.uno.ai.planning.ss.StateSpaceProblem;
 
 public class PlanSpaceRoot extends PlanSpaceNode {
 
   public final Problem problem;
+  private final AdditiveHeuristic additiveHeuristic;
 
   int limit = -1;
 
   PlanSpaceRoot(Problem problem) {
     super(problem);
     this.problem = problem;
+    additiveHeuristic = new AdditiveHeuristic(stateSpaceProblem());
   }
 
   @Override
@@ -30,7 +34,16 @@ public class PlanSpaceRoot extends PlanSpaceNode {
     return true;
   }
 
+  public Problem problem() {
+    return problem;
+  }
+
   void setNodeLimit(int limit) {
     this.limit = limit;
+  }
+
+  @Override
+  public StateSpaceProblem stateSpaceProblem() {
+    return new StateSpaceProblem(problem());
   }
 }
