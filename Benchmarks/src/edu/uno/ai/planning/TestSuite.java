8fc82a83c3d697f9eb897515501d561502fcde62
GlobalConfusion <montgomery.connor@gmail.com>
2015-12-08 08:05:34 -0600

    Properly working GP
    final commit

--- Benchmarks/src/edu/uno/ai/planning/TestSuite.java
+++ Benchmarks/src/edu/uno/ai/planning/TestSuite.java
@@ -1,22 +1,23 @@
 package edu.uno.ai.planning;
 
 import edu.uno.ai.planning.Planner;
+import hsp2.HSPlanner;
 
 public class TestSuite {
 
-	public static final int NODE_LIMIT = 10000;
+	public static final int NODE_LIMIT = 1000000;
 	public static final long TIME_LIMIT = Planner.NO_TIME_LIMIT;
 
 	public static final Planner<?>[] PLANNERS = new Planner[]{
-		new edu.uno.ai.planning.bfs.BFSPlanner(),
-		new edu.uno.ai.planning.iw.IteratedWidthPlanner(),
-		new edu.uno.ai.planning.pop.PartialOrderPlanner(),
-		new edu.uno.ai.planning.spop.PartialOrderPlanner(),
+//		new edu.uno.ai.planning.bfs.BFSPlanner(),
+//		new edu.uno.ai.planning.iw.IteratedWidthPlanner(),
+//		new edu.uno.ai.planning.pop.PartialOrderPlanner(),
+//		new edu.uno.ai.planning.spop.PartialOrderPlanner(),
 		new edu.uno.ai.planning.graphplan.Graphplan(),
 		new edu.uno.ai.planning.gp.GraphPlan(),
-		new edu.uno.ai.planning.hsp.HSPlanner(),
-		new edu.uno.ai.planning.shsp.HeuristicSearchPlanner(),
-		new edu.uno.ai.planning.ff.FastForwardPlanner(),
+//		new HSPlanner(),
+//		new edu.uno.ai.planning.shsp.HeuristicSearchPlanner(),
+//		new edu.uno.ai.planning.ff.FastForwardPlanner(),
 	};
 	
 	public static final Benchmark[] BENCHMARKS = new Benchmark[]{
@@ -25,7 +26,6 @@ public class TestSuite {
 		new Benchmark("blocks", "easy_stack"),
 		new Benchmark("blocks", "easy_unstack"),
 		new Benchmark("blocks", "sussman"),
-		new Benchmark("cake", "have_eat_cake"),
 		new Benchmark("blocks", "reverse_2"),
 		new Benchmark("blocks", "reverse_4"),
 		new Benchmark("blocks", "reverse_6"),
