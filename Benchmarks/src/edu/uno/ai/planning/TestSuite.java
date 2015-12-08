9cc2f230558f5cd9332a1d64e2bbe44ffe6b902b
Shane McCulley <smcculle@uno.edu>
2015-12-07 18:46:27 -0600

    fixing testsuite

--- Benchmarks/src/edu/uno/ai/planning/TestSuite.java
+++ Benchmarks/src/edu/uno/ai/planning/TestSuite.java
@@ -1,23 +1,32 @@
 package edu.uno.ai.planning;
 
 import edu.uno.ai.planning.Planner;
-import edu.uno.ai.planning.bfs.BFSPlanner;
 
 public class TestSuite {
 
 	public static final int NODE_LIMIT = 10000;
 	public static final long TIME_LIMIT = Planner.NO_TIME_LIMIT;
-	
+
 	public static final Planner<?>[] PLANNERS = new Planner[]{
-		new BFSPlanner(),
+		new edu.uno.ai.planning.bfs.BFSPlanner(),
+		new edu.uno.ai.planning.iw.IteratedWidthPlanner(),
+		new edu.uno.ai.planning.pop.PartialOrderPlanner(),
+		new edu.uno.ai.planning.spop.PartialOrderPlanner(),
+		new edu.uno.ai.planning.graphplan.Graphplan(),
+		new edu.uno.ai.planning.gp.GraphPlan(),
+		new edu.uno.ai.planning.hsp.HSPlanner(),
+		new edu.uno.ai.planning.shsp.HeuristicSearchPlanner(),
+		new edu.uno.ai.planning.ff.FastForwardPlanner(),
 		new edu.uno.ai.planning.lpg.LPGPlanner(),
 	};
 	
 	public static final Benchmark[] BENCHMARKS = new Benchmark[]{
+		new Benchmark("cake", "have_eat_cake"),
 		new Benchmark("blocks", "do_nothing"),
 		new Benchmark("blocks", "easy_stack"),
 		new Benchmark("blocks", "easy_unstack"),
 		new Benchmark("blocks", "sussman"),
+		new Benchmark("cake", "have_eat_cake"),
 		new Benchmark("blocks", "reverse_2"),
 		new Benchmark("blocks", "reverse_4"),
 		new Benchmark("blocks", "reverse_6"),
